fcd36f67135aa29846ba11794a0f394a33120c69
Joshua <Joshua@Admins-MacBook.local>
2018-02-11 22:57:02 -0600

    adding file structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
